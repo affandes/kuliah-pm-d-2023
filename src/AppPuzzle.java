38f4fba0bb99bc7a0d1f909a1e9efe4e4b9a20ca
Muhammad Affandes <affandes@uin-suska.ac.id>
2023-10-06 11:25:22 +0700

    Update

--- src/AppPuzzle.java
+++ src/AppPuzzle.java
@@ -2,24 +2,23 @@ public class AppPuzzle {
     public static void main(String[] args) {
 
         Puzzle puzzle = new Puzzle();
-        puzzle.data[0] = 1;
-        puzzle.data[1] = 2;
+        puzzle.data[0] = 4;
+        puzzle.data[1] = 0;
         puzzle.data[2] = 3;
-        puzzle.data[3] = 4;
+        puzzle.data[3] = 2;
         puzzle.data[4] = 5;
         puzzle.data[5] = 6;
-        puzzle.data[6] = 7;
-        puzzle.data[7] = 8;
-        puzzle.data[8] = 0;
+        puzzle.data[6] = 8;
+        puzzle.data[7] = 1;
+        puzzle.data[8] = 7;
         System.out.println(puzzle);
-        try {
-            puzzle.moveUp();
-            puzzle.moveUp();
-            puzzle.moveUp();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+
+        Puzzle[] puzzles = puzzle.next();
+
+        for (int i = 0; i < puzzles.length; i++) {
+            System.out.println(puzzles[i]);
         }
-        System.out.println(puzzle);
+
 
     }
 }
--- src/Puzzle.java
+++ src/Puzzle.java
@@ -16,15 +16,135 @@ public class Puzzle {
         return -1;
     }
 
-    public void moveUp() throws Exception {
+    public Puzzle duplikatPuzzle() {
+        Puzzle baru = new Puzzle();
+        for (int i = 0; i < baru.data.length; i++) {
+            baru.data[i] = data[i];
+        }
+        return baru;
+    }
+
+    public Puzzle moveUp() throws Exception {
+        Puzzle baru = duplikatPuzzle();
         int indexPosisiKosong = cariIndexKosong();
         if (indexPosisiKosong > 2) {
             // Pindahkan antara id ? - ?-3
-            int temp = data[indexPosisiKosong];
-            data[indexPosisiKosong] = data[indexPosisiKosong-3];
-            data[indexPosisiKosong-3] = temp;
+            int temp = baru.data[indexPosisiKosong];
+            baru.data[indexPosisiKosong] = baru.data[indexPosisiKosong-3];
+            baru.data[indexPosisiKosong-3] = temp;
         } else {
             throw new Exception("Ndak bisa");
         }
+        return baru;
     }
+
+    public Puzzle moveDown() throws Exception {
+        Puzzle baru = duplikatPuzzle();
+        int indexPosisiKosong = cariIndexKosong();
+        if (indexPosisiKosong < 6) {
+            // Pindahkan antara id ? - ?-3
+            int temp = baru.data[indexPosisiKosong];
+            baru.data[indexPosisiKosong] = baru.data[indexPosisiKosong+3];
+            baru.data[indexPosisiKosong+3] = temp;
+        } else {
+            throw new Exception("Ndak bisa");
+        }
+        return baru;
+    }
+
+    public Puzzle moveLeft() throws Exception {
+        Puzzle baru = duplikatPuzzle();
+        int indexPosisiKosong = cariIndexKosong();
+        if (indexPosisiKosong != 6 && indexPosisiKosong != 3 && indexPosisiKosong != 0) {
+            int temp = baru.data[indexPosisiKosong];
+            baru.data[indexPosisiKosong] = baru.data[indexPosisiKosong - 1];
+            baru.data[indexPosisiKosong - 1] = temp;
+        } else {
+            throw new Exception("Ada-ada saja");
+        }
+        return baru;
+    }
+
+    public Puzzle moveRight() throws Exception {
+        Puzzle baru = duplikatPuzzle();
+        int indexPosisiKosong = cariIndexKosong();
+        if (indexPosisiKosong != 2 && indexPosisiKosong != 5 && indexPosisiKosong != 8) {
+            int temp = baru.data[indexPosisiKosong];
+            baru.data[indexPosisiKosong] = baru.data[indexPosisiKosong + 1];
+            baru.data[indexPosisiKosong + 1] = temp;
+        } else {
+            throw new Exception("Ada-ada saja");
+        }
+        return baru;
+    }
+
+    public Puzzle[] next() {
+
+        Puzzle[] puzzles;
+        int indexPosisiKosong = cariIndexKosong();
+        try {
+            switch (indexPosisiKosong) {
+                case 0:
+                    puzzles = new Puzzle[2];
+                    puzzles[0] = moveDown();
+                    puzzles[1] = moveRight();
+                    break;
+                case 1:
+                    puzzles = new Puzzle[3];
+                    puzzles[0] = moveDown();
+                    puzzles[1] = moveLeft();
+                    puzzles[2] = moveRight();
+                    break;
+                case 2:
+                    puzzles = new Puzzle[2];
+                    puzzles[0] = moveDown();
+                    puzzles[1] = moveLeft();
+                    break;
+                case 3:
+                    puzzles = new Puzzle[3];
+                    puzzles[0] = moveUp();
+                    puzzles[1] = moveDown();
+                    puzzles[2] = moveRight();
+                    break;
+                case 4:
+                    puzzles = new Puzzle[4];
+                    puzzles[0] = moveUp();
+                    puzzles[1] = moveDown();
+                    puzzles[2] = moveLeft();
+                    puzzles[3] = moveRight();
+                    break;
+                case 5:
+                    puzzles = new Puzzle[3];
+                    puzzles[0] = moveUp();
+                    puzzles[1] = moveDown();
+                    puzzles[2] = moveLeft();
+                    break;
+                case 6:
+                    puzzles = new Puzzle[2];
+                    puzzles[0] = moveUp();
+                    puzzles[1] = moveRight();
+                    break;
+                case 7:
+                    puzzles = new Puzzle[3];
+                    puzzles[0] = moveUp();
+                    puzzles[1] = moveLeft();
+                    puzzles[2] = moveRight();
+                    break;
+                case 8:
+                    puzzles = new Puzzle[2];
+                    puzzles[0] = moveUp();
+                    puzzles[1] = moveLeft();
+                    break;
+                default:
+                    puzzles = new Puzzle[]{};
+            }
+        } catch (Exception e) {
+            puzzles = new Puzzle[]{};
+            System.out.println(e.getMessage());
+        }
+
+        return puzzles;
+    }
+
+
 }
